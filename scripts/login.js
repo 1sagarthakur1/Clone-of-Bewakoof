504448fce6a6933250bb9e3194c3e09464f3c9d8
Sagar Thakur <sagarthakur5710@gmail.com>
2022-08-25 22:20:14 +0530

    day3

--- scripts/login.js
+++ scripts/login.js
@@ -1,3 +1,4 @@
+var arr = ["1232","6267986053"]
 function forloging(){
     event.preventDefault()
     let num = document.querySelector("#inputbala").value;
@@ -9,7 +10,24 @@ function forloging(){
             alert("invaild Number ")
         }
         else{
-            console.log(num)
+            // console.log(num)
+            window.location.href="otp.html"
+            function generateOTP() {
+                var digits = '0123456789';
+                let OTP = '';
+                for (let i = 0; i < 4; i++ ) {
+                    OTP += digits[Math.floor(Math.random() * 10)];
+                }
+                return OTP;
+            }
+            var otp = generateOTP()
+            alert("one-time password : "+ otp)
+            arr.shift()
+            arr.shift()
+            arr.push(otp)
+            arr.push(num)
+            console.log(arr)
+            localStorage.setItem("OTP",JSON.stringify(arr))
         }
     }
 }
